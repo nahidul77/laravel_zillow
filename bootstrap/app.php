8651144d4a4fe64be6e96dfcf4e7a3ea76cfef2a
binpoet <ghawkinst0@gmail.com>
2024-12-28 08:59:07 -0600

    add first vue page

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Foundation\Application;
+use App\Http\Middleware\HandleInertiaRequests;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
