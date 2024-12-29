24a1563fa3da1b84ea2e6223bced84317821f6f3
Nahidul Islam <contact.nahidul@gmail.com>
2024-12-29 11:16:37 -0600

    passing data to pages

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -8,9 +8,11 @@ class IndexController extends Controller
 {
     public function index()
     {
-        return inertia('Index/Index');
+        return inertia('Index/Index', [
+            'message' => 'Hello from Laravel'
+        ]);
     }
-    
+
     public function show()
     {
         return inertia('Index/Show');
--- resources/js/Pages/Index/Index.vue
+++ resources/js/Pages/Index/Index.vue
@@ -1,8 +1,15 @@
 <template>
     <div>Index</div>
     <Link href="/hello">Show Page</Link>
+    <div>
+        {{ message }}
+    </div>
 </template>
 
 <script setup>
   import { Link } from '@inertiajs/vue3'
+
+  defineProps({
+    message: String
+  });
 </script>
